7431b31fc26c1e607e58ebdf12d67138477c9ea1
Pavel Gabriel <alovak@gmail.com>
2023-06-13 20:49:58 +0200

    add tests for ranges

--- formats/iso0.go
+++ formats/iso0.go
@@ -18,10 +18,19 @@ func NewISO0() *ISO0 {
 
 // Encode returns the ISO0 PIN block for the given PIN and account number
 func (i *ISO0) Encode(pin string, account string) (string, error) {
+	if len(pin) < 4 || len(pin) > 12 {
+		return "", fmt.Errorf("pin length must be between 4 and 12 digits")
+	}
+
 	// pin block should start with 0, then add length of pin, then add pin,
 	// then add F until 16 characters
 	padding := strings.Repeat(i.Filler, 14-len(pin))
 	pinBlock := fmt.Sprintf("0%d%s%s", len(pin), pin, padding)
+
+	// account number must be at least 13 digits, including the check digit
+	if len(account) < 13 {
+		return "", fmt.Errorf("account length must be at least 13 digits")
+	}
 
 	// take the last 12 digits of the account number excluding the check digit
 	accountBlock := fmt.Sprintf("0000%s", account[len(account)-13:len(account)-1])
@@ -35,6 +44,14 @@ func (i *ISO0) Encode(pin string, account string) (string, error) {
 }
 
 func (i *ISO0) Decode(pinBlock string, account string) (string, error) {
+	if len(pinBlock) != 16 {
+		return "", fmt.Errorf("pin block must be 16 characters")
+	}
+
+	if len(account) < 13 {
+		return "", fmt.Errorf("account length must be at least 13 digits")
+	}
+
 	// take the last 12 digits of the account number excluding the check digit
 	accountBlock := fmt.Sprintf("0000%s", account[len(account)-13:len(account)-1])
 
--- formats/iso0_test.go
+++ formats/iso0_test.go
@@ -7,22 +7,94 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestISO0Encode(t *testing.T) {
-	pin := "1234"
-	account := "5432101234567891"
+func TestISO0(t *testing.T) {
+	t.Run("Encode", func(t *testing.T) {
+		pin := "1234"
+		account := "5432101234567891"
 
-	iso0 := formats.NewISO0()
-	pinBlock, err := iso0.Encode(pin, account)
+		iso0 := formats.NewISO0()
+		pinBlock, err := iso0.Encode(pin, account)
 
-	require.NoError(t, err)
+		require.NoError(t, err)
 
-	// can be checked here:
-	// https://paymentcardtools.com/pin-block-calculators/iso9564-format-0
-	require.Equal(t, "041215FEDCBA9876", pinBlock)
+		// can be checked here:
+		// https://paymentcardtools.com/pin-block-calculators/iso9564-format-0
+		require.Equal(t, "041215FEDCBA9876", pinBlock)
+	})
 
-	// test Decode
-	pin, err = iso0.Decode(pinBlock, account)
-	require.NoError(t, err)
+	t.Run("bad pin length", func(t *testing.T) {
+		iso0 := formats.NewISO0()
 
-	require.Equal(t, "1234", pin)
+		account := "5432101234567891"
+
+		// test short pin
+		shortPin := "123"
+
+		_, err := iso0.Encode(shortPin, account)
+
+		require.Error(t, err)
+
+		// test long pin
+		longPin := "1234567890123"
+
+		_, err = iso0.Encode(longPin, account)
+
+		require.Error(t, err)
+	})
+
+	t.Run("bad account length", func(t *testing.T) {
+		iso0 := formats.NewISO0()
+		pin := "1234"
+
+		// test short account
+		shortAccount := "456789"
+
+		_, err := iso0.Encode(pin, shortAccount)
+
+		require.Error(t, err)
+	})
+}
+
+func TestISO0_Decode(t *testing.T) {
+	t.Run("Decode", func(t *testing.T) {
+		account := "5432101234567891"
+		pinBlock := "041215FEDCBA9876"
+
+		// test Decode
+		iso0 := formats.NewISO0()
+		pin, err := iso0.Decode(pinBlock, account)
+		require.NoError(t, err)
+
+		require.Equal(t, "1234", pin)
+	})
+
+	t.Run("bad pin block length", func(t *testing.T) {
+		iso0 := formats.NewISO0()
+		account := "5432101234567891"
+
+		// test short pin block
+		shortPinBlock := "041215FEDCBA987"
+
+		_, err := iso0.Decode(shortPinBlock, account)
+
+		require.Error(t, err)
+
+		// test long pin block
+		longPinBlock := "041215FEDCBA98765"
+
+		_, err = iso0.Decode(longPinBlock, account)
+
+		require.Error(t, err)
+	})
+
+	t.Run("bad account length", func(t *testing.T) {
+		iso0 := formats.NewISO0()
+
+		// test short account
+		shortAccount := "456789"
+
+		_, err := iso0.Decode("041215FEDCBA9876", shortAccount)
+
+		require.Error(t, err)
+	})
 }
